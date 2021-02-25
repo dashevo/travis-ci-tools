62b270b88865a6bbec9736a39cf01addefde3c8b
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-02-25 20:11:02 +0300

    feat: update start script to work with mn-bootstrap 0.19

--- bin/start-local-node.sh
+++ bin/start-local-node.sh
@@ -10,7 +10,6 @@ Usage: start-local-node.sh <path-to-package.json> [options]
   Options:
   --override-major-version    - major version to use
   --override-minor-version    - minor version to use
-  --node-count                - number of masternodes (default is 3)
   --dapi-branch               - dapi branch to be injected into mn-bootstrap
   --drive-branch              - drive branch to be injected into mn-bootstrap
   --sdk-branch                - Dash SDK (DashJS) branch to be injected into mn-bootstrap
@@ -47,9 +46,6 @@ case ${i} in
     ;;
     --sdk-branch=*)
     sdk_branch="${i#*=}"
-    ;;
-    --node-count=*)
-    node_count="${i#*=}"
     ;;
 esac
 done
@@ -106,34 +102,48 @@ then
   npm i "github:dashevo/DashJS#$sdk_branch"
 fi
 
-if [ -z "$node_count" ]
-then
-  node_count=3
-fi
-
 npm link
 
 # Setup node for local node mn-bootstrap
 echo "Setting up a local node"
 
-OUTPUT=$(mn setup local --node-count="$node_count" "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options")
+# Set number of nodes
+NODE_COUNT=3
+
+if [[ $CURRENT_VERSION == "0.19"* ]]
+then
+  OUTPUT=$(mn setup local --node-count="$NODE_COUNT" "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options")
+  CONFIG="local_1"
+else
+  mn config:default local
+  OUTPUT=$(mn setup local "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options")
+  CONFIG="local"
+fi
 
 FAUCET_PRIVATE_KEY=$(echo "$OUTPUT" | grep -m 1 "Private key:" | awk '{printf $4}')
-DPNS_CONTRACT_ID=$(mn config:get --config=local_1 platform.dpns.contract.id)
-DPNS_CONTRACT_BLOCK_HEIGHT=$(mn config:get --config=local_1 platform.dpns.contract.blockHeight)
-DPNS_TOP_LEVEL_IDENTITY_ID=$(mn config:get --config=local_1 platform.dpns.ownerId)
+DPNS_CONTRACT_ID=$(mn config:get --config="$CONFIG" platform.dpns.contract.id)
+DPNS_CONTRACT_BLOCK_HEIGHT=$(mn config:get --config="$CONFIG" platform.dpns.contract.blockHeight)
+DPNS_TOP_LEVEL_IDENTITY_ID=$(mn config:get --config="$CONFIG" platform.dpns.ownerId)
 DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY=$(echo "$OUTPUT" | grep -m 1 "HD private key:" | awk '{$1=""; printf $5}')
 
-# Settings for masternodes
-for (( i=1; i<=node_count; i++ ))
-do
-    mn config:set --config=local_"${i}" environment development
-    mn config:set --config=local_"${i}" platform.drive.abci.log.stdout.level trace
-done
+if [[ $CURRENT_VERSION == "0.19"* ]]
+then
+  # Settings for masternodes
+  for (( i=1; i<=NODE_COUNT; i++ ))
+  do
+      mn config:set --config=local_"${i}" environment development
+      mn config:set --config=local_"${i}" platform.drive.abci.log.stdout.level trace
+  done
 
-# Settings for seed node
-mn config:set --config=local_seed core.miner.enable true
-mn config:set --config=local_seed core.miner.interval 1s
+  # Settings for seed node
+  mn config:set --config=local_seed core.miner.enable true
+  mn config:set --config=local_seed core.miner.interval 1s
+else
+  mn config:set core.miner.enable true
+  mn config:set core.miner.interval 1s
+  mn config:set environment development
+  mn config:set platform.drive.abci.log.stdout.level trace
+fi
 
 echo "Node is configured:"
 
@@ -146,7 +156,12 @@ echo "DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY: ${DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY
 
 #Start mn-bootstrap
 echo "Starting mn-bootstrap"
-mn group:start "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options"
+if [[ $CURRENT_VERSION == "0.19"* ]]
+then
+  mn group:start "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options"
+else
+  mn start "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options"
+fi
 
 #Export variables
 export CURRENT_VERSION
